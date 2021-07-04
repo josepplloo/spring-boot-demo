39a605fe7b75bc72ede4116fd4c282f0943bc098
Jose Garcia <Jose.Garcia@teaminternational.com>
2021-07-04 18:05:24 -0500

    add another endpoint for greetings

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # spring-boot-demo
 
 Tutorial based in the rest service guide provided by [Spring.io](https://spring.io/guides/gs/rest-service/)
+
+# Build & Run  this project
+Install the JDK stuff and clone this repo.
+
+execute `./gradlew build` for build.
+
+execute `./gradlew bootRun` for run, go to your browser or `curl` for `http://localhost:8080/hello`. eg `http://localhost:8080/greeting?name=josepplloo`
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "com.example"} )
 @RestController
 public class DemoApplication {
 	public static void main(String[] args) {
@@ -14,7 +14,7 @@ public class DemoApplication {
 	}
 
 	@GetMapping("/hello")
-		public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-			return String.format("Hello %s!", name);
-		}
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
